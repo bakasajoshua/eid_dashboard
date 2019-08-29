9d96a2d939a883de7bd573ca9e6a2478f7a0d44f
Shadrack-B <shadrackbakasa5@gmail.com>
2019-08-29 09:19:35 +0300

    config

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
@@ -118,7 +118,6 @@ switch (ENVIRONMENT)
 	// $application_folder = 'application';
 	$application_folder = 'application';
 
-	$application_folder = '/var/www/eid_dir/eid_dashboard/application';
 
 /*
  *---------------------------------------------------------------
